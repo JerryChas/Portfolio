11d39d43425f4ebf338f2df378103b0b84b71a51
jerryChas <jerry.olsson@chasacademy.se>
2024-10-07 00:24:43 +0200

    style to projectpage

--- src/pages/projectsPage/ProjectsPage.tsx
+++ src/pages/projectsPage/ProjectsPage.tsx
@@ -3,8 +3,11 @@ import projects from '../../assets/projects.json';
 
 const ProjectsPage = () => {
   return (
-    <div className='flex flex-col gap-4 mx-auto my-6 px-1 container'>
-      <h2 className='font-bold text-3xl text-center italic'>School Projects</h2>
+    <div className='flex flex-col gap-8 bg-background p-4 md:p-8 w-full text-text'>
+      <h2 className='font-bold text-3xl text-accent text-center md:text-4xl'>
+        Projects
+      </h2>
+
       {projects.map((project, index: number) => (
         <ProjectCard data={project} key={index} even={index % 2 === 0} />
       ))}
